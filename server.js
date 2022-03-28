d40285a459b84c0ca7381d8733e5f7669bc80372
ekh <52539130+eomkyeongho@users.noreply.github.com>
2022-03-29 02:48:41 +0900

    Part3 - 7

--- server.js
+++ server.js
@@ -2,19 +2,29 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const { MongoDBNamespace } = require('mongodb');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+require('dotenv').config();
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 app.use('/public', express.static('public'));
+app.use(session({secret : 'sec', resave : true, saveUninitialized : false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 var db;
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
-MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.scygd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(err, client)
+MongoClient.connect(process.env.DB_URL, function(err, client)
 {
     if (err) return console.log(err);
 
     db = client.db('ToDoApp');
 
-    app.listen(8080, function() 
+    app.listen(process.env.PORT, function() 
     {
         console.log('listening on 8080...');
     });
@@ -25,7 +35,7 @@ app.get('/', function(request, response)
     response.render('index.ejs');
 });
 
-app.get('/write', function(request, response)
+app.get('/write', verification, function(request, response)
 {
     response.render('write.ejs');
 });
@@ -38,20 +48,21 @@ app.get('/list', function(request, response)
     });
 });
 
-app.post('/add', function(request, response)
+app.post('/add', verification, function(request, response)
 {
     db.collection('counter').findOne({name:'postNum'}, function(err, result)
     {
         db.collection('post').insertOne({_id : result.totalPost + 1, title : request.body.title, date : request.body.date}, function(err, result)
         {
-            db.collection('counter').updateOne({name:'postNum'}, { $inc : {totalPost:1}}, function(){})
+            db.collection('counter').updateOne({name:'postNum'}, { $inc : {totalPost:1}}, function()
+            {
+                response.redirect('/list');
+            });
         });
     });
-    response.write("<script>alert('Success')</script>");
-    response.write("<script>window.location=\"/list\"</script>");
 });
 
-app.delete('/delete', function(request, response)
+app.delete('/delete', verification, function(request, response)
 {
     request.body._id = parseInt(request.body._id);
     db.collection('post').deleteOne(request.body, function(err, result)
@@ -60,10 +71,87 @@ app.delete('/delete', function(request, response)
     });
 });
 
-app.get('/detail/:id', function(request, response)
+app.get('/detail/:id', verification, function(request, response)
 {
     db.collection('post').findOne({ _id : parseInt(request.params.id) }, function(err, result)
     {
         response.render('detail.ejs', { data : result});
+    });
+});
+
+app.get('/edit/:id', verification, function(request, response)
+{
+    db.collection('post').findOne({_id:parseInt(request.params.id)}, function(err, result)
+    {
+        response.render('edit.ejs', { data : result});
+    });
+});
+
+app.put('/edit', verification, function(request, response)
+{
+    db.collection('post').updateOne({ _id : parseInt(request.body._id)}, { $set : { title : request.body.title, date : request.body.date} }, function(err, result)
+    {
+        response.redirect(`/detail/${request.body._id}`);
+    });
+});
+
+app.get('/login', function(request, response)
+{
+    response.render('login.ejs');
+});
+
+app.post('/login', passport.authenticate('local', {failureRedirect : '/fail'}), function(request, response)
+{
+    response.redirect('/');
+});
+
+app.get('/mypage', verification, function(request, response)
+{
+    response.render('mypage.ejs', { data : request.user.result});
+});
+
+app.get('/fail', function(request, response)
+{
+    response.send("<script>alert('유효하지 않은 로그인 정보입니다.'); window.location=\"/login\"</script>");
+});
+
+function verification(request, response, next)
+{
+    if(request.user) { next(); }
+    else { response.redirect('/login'); }
+}
+
+passport.use(new LocalStrategy( // 로그인 유효성 검사
+{
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+}, function (input_id, input_pw, done) 
+{
+    db.collection('login').findOne({ id: input_id }, function (err, result) 
+    {
+      if (err) return done(err)
+  
+      if (!result) return done(null, false, { message: 'Not exist' })
+
+      if (input_pw == result.pw) {
+        return done(null, result)
+      } else {
+        return done(null, false, { message: 'Invalid Password' })
+      }
+    })
+}));
+
+passport.serializeUser(function(user, done)
+{
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) // login 사용자 정보 가져올 때 사용
+{
+    db.collection('login').findOne({id : id}, function(err, result)
+    {
+        done(null, {result});
     });
 });
